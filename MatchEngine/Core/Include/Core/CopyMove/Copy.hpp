8e4e9b940be1f4f6d9a5fedf766ea2624595f92a
HeaoYe <1697427528@qq.com>
2024-02-23 23:14:35 +0800

    禁止Copy右值引用

--- MatchEngine/Core/Include/Core/CopyMove/Copy.hpp
+++ MatchEngine/Core/Include/Core/CopyMove/Copy.hpp
@@ -4,12 +4,20 @@
 
 namespace MatchEngine::Core {
     template <typename T>
-    const std::decay_t<T> &Copy(T &&t) {
+    const std::decay_t<T> &Copy(T &t) {
         return static_cast<const std::decay_t<T> &>(t);
     }
 
     template <typename T>
-    const std::decay_t<T> &Copy(const T &&t) {
+    const std::decay_t<T> &Copy(const T &t) {
+        return static_cast<const std::decay_t<T> &>(t);
+    }
+
+    template <typename T>
+    const std::decay_t<T> &Copy(T &&t) {
+        if constexpr (std::is_rvalue_reference_v<T &&>) {
+            static_assert(0, "不可以复制右值引用");
+        }
         return static_cast<const std::decay_t<T> &>(t);
     }
 }
