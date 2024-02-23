cd872f2e4c4d6c8cccd241f3a82b4c8b78fb8dc9
HeaoYe <1697427528@qq.com>
2024-02-23 23:49:09 +0800

    添加TMap

---
+++ MatchEngine/Core/Include/Core/Container/Map.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "Core/Defines.hpp"
+#include "Core/CopyMove/CopyMove.hpp"
+#include "Pair.hpp"
+
+#include <type_traits>
+
+#include <map>
+
+namespace MatchEngine::Core {
+    /**
+     * @brief 键值对容器, 相当于std::map
+     *
+     * @tparam _Key 键的数据类型
+     * @tparam _Value 值的数据类型
+     * @tparam Compare 键的数据的比较方式, 默认为std::less
+     *
+     * TODO: 不依赖std::map进行实现
+     * TODO: 进行内存管理
+     */
+    template <typename _Key, typename _Value, typename Compare = std::less<_Key>>
+    class TMap : std::map<_Key, _Value> {
+        using Key = std::decay_t<_Key>;
+        using Value = std::decay_t<_Value>;
+        using KeyValuePair = TPair<const Key, Value>;
+        using size_type = size_t;
+    public:
+        TMap() : std::map<_Key, _Value, Compare>() {}
+        TMap(const TMap &other) : std::map<_Key, _Value, Compare>(Copy(other)) {}
+        TMap(TMap &&other) : std::map<_Key, _Value, Compare>(Move(other)) {}
+        DefineDefaultAssignmentOperator(TMap)
+
+        TMap(std::initializer_list<std::pair<const Key, Value>> initializer_list) : std::map<_Key, _Value, Compare>(initializer_list) {}
+    public:
+        void add(const Key &key, const Value &value) {
+            std::map<_Key, _Value, Compare>::insert(key, value);
+        }
+
+        void remove(const Key &key) {
+            std::map<_Key, _Value, Compare>::erase(key);
+        }
+
+        size_type size() const {
+            return std::map<_Key, _Value, Compare>::size();
+        }
+
+        void clear() {
+            std::map<_Key, _Value, Compare>::clear();
+        }
+
+        auto begin() {
+            return std::map<_Key, _Value, Compare>::begin();
+        }
+
+        auto end() {
+            return std::map<_Key, _Value, Compare>::end();
+        }
+
+        Value &at(const Key &key) {
+            return std::map<_Key, _Value, Compare>::at(key);
+        }
+
+        const Value &at(const Key &key) const {
+            return std::map<_Key, _Value, Compare>::at(key);
+        }
+    };
+}
