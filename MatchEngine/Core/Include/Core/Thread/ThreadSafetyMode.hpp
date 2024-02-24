c50d1cd2e3cc4c8e648cc1ecb7d931ec35b9f0ab
HeaoYe <1697427528@qq.com>
2024-02-24 12:24:27 +0800

    添加结构体形式的ThreadSafetyMode

--- MatchEngine/Core/Include/Core/Thread/ThreadSafetyMode.hpp
+++ MatchEngine/Core/Include/Core/Thread/ThreadSafetyMode.hpp
@@ -4,9 +4,32 @@ namespace MatchEngine::Core {
     /**
      * @brief 线程安全模式, 用于区分一个类的两种版本
      *
+     * 用法: template <..., ThreadSafetyMode mode>
+     *
      */
     enum class ThreadSafetyMode {
         eThreadSafe,
         eNotThreadSafe,
     };
+
+    /**
+     * @brief 线程安全结模式构体, 不是线程安全
+     *
+     * 用法: template <..., typename ...Ts, typename ThreadSafetyModeStruct>
+     *
+     */
+    struct ThreadSafetyModeNotThreadSafeStruct {
+        static constexpr ThreadSafetyMode SafetyMode = ThreadSafetyMode::eNotThreadSafe;
+    };
+
+    /**
+     * @brief 线程安全结模式构体, 线程安全
+     *
+     * 用法: template <..., typename ...Ts, typename ThreadSafetyModeStruct>
+     *
+     */
+    struct ThreadSafetyModeThreadSafeStruct {
+        static constexpr ThreadSafetyMode SafetyMode = ThreadSafetyMode::eThreadSafe;
+    };
+
 }
