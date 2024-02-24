c4b59f1b29f75f89ecb2840a5b0d67ab8b0001fd
HeaoYe <1697427528@qq.com>
2024-02-24 10:33:47 +0800

    noexcept

--- MatchEngine/Core/Include/Core/CopyMove/Copy.hpp
+++ MatchEngine/Core/Include/Core/CopyMove/Copy.hpp
@@ -4,19 +4,19 @@
 
 namespace MatchEngine::Core {
     template <typename T>
-    const std::decay_t<T> &Copy(T &t) {
+    const std::decay_t<T> &Copy(T &t) noexcept {
         return static_cast<const std::decay_t<T> &>(t);
     }
 
     template <typename T>
-    const std::decay_t<T> &Copy(const T &t) {
+    const std::decay_t<T> &Copy(const T &t) noexcept {
         return static_cast<const std::decay_t<T> &>(t);
     }
 
     template <typename T>
-    const std::decay_t<T> &Copy(T &&t) {
+    const std::decay_t<T> &Copy(T &&t) noexcept {
         if constexpr (std::is_rvalue_reference_v<T &&>) {
-            static_assert(0, "不可以复制右值引用");
+            static_assert(0, "Copy: 不可以复制右值引用");
         }
         return static_cast<const std::decay_t<T> &>(t);
     }
--- MatchEngine/Core/Include/Core/CopyMove/Move.hpp
+++ MatchEngine/Core/Include/Core/CopyMove/Move.hpp
@@ -4,7 +4,7 @@
 
 namespace MatchEngine::Core {
     template <typename T>
-    std::decay_t<T> &&Move(T &&t) {
+    std::decay_t<T> &&Move(T &&t) noexcept {
         return static_cast<std::decay_t<T> &&>(t);
     };
 }
