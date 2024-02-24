144a6e988aefe1fc0e659a31888f8e33dca46449
HeaoYe <67638761+HeaoYe@users.noreply.github.com>
2024-02-24 15:58:40 +0800

    更改整形句柄分配器类型

--- MatchEngine/Core/Include/Core/Misc/SpecHandleAllocator.hpp
+++ MatchEngine/Core/Include/Core/Misc/SpecHandleAllocator.hpp
@@ -10,22 +10,22 @@ namespace MatchEngine::Core {
      *
      * @tparam mode 线程安全模式
      */
-    template <ThreadSafetyMode mode>
-    using U32HandleAllocator = THandleAllocator<uint32_t, mode>;
+    template <ThreadSafetyMode mode = ThreadSafetyMode::eThreadSafe>
+    using TU32HandleAllocator = THandleAllocator<uint32_t, mode>;
 
     /**
      * @brief 64位无符号整数句柄分配器
      *
      * @tparam mode 线程安全模式
      */
-    template <ThreadSafetyMode mode>
-    using U64HandleAllocator = THandleAllocator<uint64_t, mode>;
+    template <ThreadSafetyMode mode = ThreadSafetyMode::eThreadSafe>
+    using TU64HandleAllocator = THandleAllocator<uint64_t, mode>;
 
     /**
      * @brief 无符号整数句柄分配器
      *
      * @tparam mode 线程安全模式
      */
-    template <ThreadSafetyMode mode>
-    using UIntHandleAllocator = THandleAllocator<std::size_t, mode>;
+    template <ThreadSafetyMode mode = ThreadSafetyMode::eThreadSafe>
+    using TUIntHandleAllocator = THandleAllocator<size_t, mode>;
 }
