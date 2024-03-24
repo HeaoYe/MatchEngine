469056ae729e9d0a905df5e2b0e3acaad1138cd2
HeaoYe <1697427528@qq.com>
2024-03-24 11:37:53 +0800

    添加Core/Core.hpp

---
+++ MatchEngine/Core/Include/Core/Core.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+// IWYU pragma: begin_exports
+
+#include "Container/Container.hpp"
+#include "Delegate/Delegate.hpp"
+#include "Math/Math.hpp"
+#include "Misc/Misc.hpp"
+#include "SharedPtr/SharedPtr.hpp"
+#include "String/String.hpp"
+#include "I18n/I18n.hpp"
+#include "Thread/Thread.hpp"
+
+// IWYU pragma: end_exports
