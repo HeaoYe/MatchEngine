6fb885b40a6f4df6ef017e5eb869b034df09a025
HeaoYe <1697427528@qq.com>
2024-03-25 11:03:09 +0800

    Code Review

--- MatchEngine/Core/Include/Core/Thread/Atomic.hpp
+++ MatchEngine/Core/Include/Core/Thread/Atomic.hpp
@@ -15,9 +15,11 @@ namespace MatchEngine::Core {
     public:
         TAtomic() = default;
         TAtomic(T value) : data(value) {}
-        T fetchAdd() { return std::atomic_fetch_add(&data, 1); }
-        T fetchSub() { return std::atomic_fetch_sub(&data, 1); }
-        T load() { return data.load(); }
+        T fetchIncrease() { return std::atomic_fetch_add(&data, 1); }
+        T fetchDecrease() { return std::atomic_fetch_sub(&data, 1); }
+        T fetchAdd(T value) { return std::atomic_fetch_add(&data, value); }
+        T fetchSub(T value) { return std::atomic_fetch_sub(&data, value); }
+        T load() const { return data.load(); }
         void store(T value) { data.store(value); }
         T exchange(T value) { return data.exchange(value); }
         bool compareExchange(T &old_value, T value) { return data.compare_exchange_strong(old_value, value); }
